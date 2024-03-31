fb7228380758cb0140073a1d4fecd8548a3ee648
Totan Bar <totanbar42@gmail.com>
2024-03-31 20:06:19 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import "@/styles/globals.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import "../styles/globals.css";
+import {Footer, NavBar}  from "../components/componentsindex";
+const MyApp = ({ Component, pageProps }) => (
+    <div>
+    <NavBar />
+    <Component {...pageProps} />
+    <Footer />
+    </div>
+);
+export default MyApp;
